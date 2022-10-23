00bdbc8e00c080db98c57c0e4bb9ccc2cd000c31
kelly.lopes <kelly.lopes@maida.health>
2022-10-23 16:35:38 -0300

    criado container do projeto e component navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardContainerComponent } from './pages/board-container/board-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:BoardContainerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './pages/board/board.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { BoardContainerComponent } from './pages/board-container/board-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    NavbarComponent,
+    BoardContainerComponent
   ],
   imports: [
     BrowserModule,
