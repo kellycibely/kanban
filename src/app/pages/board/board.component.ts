b05bac5800b13afca2ec7abfd1bae3ac8488f4e4
kelly.lopes <kelly.lopes@maida.health>
2022-10-23 19:45:11 -0300

    adicionado models para colunas dinamicas

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/shared/models/board.model';
+import { Column } from 'src/app/shared/models/column.model';
+import { Task } from 'src/app/shared/models/task.model';
 
 @Component({
   selector: 'app-board',
@@ -8,16 +11,35 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 })
 export class BoardComponent implements OnInit {
 
+  board: Board = new Board('Sprint #1', [
+    new Column('Idea', [
+      new Task('Task Idea 1', 'Essa é a task 1', 'Priorizado'),
+      new Task('Task Idea 2', 'Essa é a task 2', 'Priorizado'),
+      new Task('Task Idea 3', 'Essa é a task 3', 'Priorizado')
+    ]),
+    new Column('Research', [
+      new Task('Task Research 1', 'Essa é a task 1', 'Research'),
+      new Task('Task Research 2', 'Essa é a task 2', 'Research'),
+      new Task('Task Research 3', 'Essa é a task 3', 'Research')
+    ]),
+    new Column('todo', [
+      new Task('Task todo 1', 'Essa é a task 1', 'tudo'),
+      new Task('Task todo 2', 'Essa é a task 2', 'tudo'),
+      new Task('Task todo 3', 'Essa é a task 3', 'tudo')
+    ]),
+    new Column('done', [
+      new Task('Task 1 done', 'Essa é a task 1', 'done'),
+      new Task('Task 2 done', 'Essa é a task 2', 'done'),
+      new Task('Task 3 done', 'Essa é a task 3', 'done')
+    ]),
+    
+  ])
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -28,6 +50,10 @@ export class BoardComponent implements OnInit {
         event.currentIndex,
       );
     }
+  }
+
+  open(item: any) {
+    console.log(item);
   }
 
 }
